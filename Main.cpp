6f9c3b600532dd03bb22949f4810455b085c1cce
nick-olesen <70233329+nick-olesen@users.noreply.github.com>
2020-08-31 21:06:53 -0500

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+/*
+Nick Olesen
+C++ Fall 2020
+Due : August 31, 2020
+Lab 1 Hello World
+Lab description: This lab is to demonstrate my ability to download a project from Github, run and compile that project, 
+upload it to Github, and lastly submit it on google classroom. 
+*/
+
+#include<iostream> 
+using namespace std;
+
+int main()
+{
+	cout << "Magenta";
+
+	return 0;
+}
